68e0d7f9daa88c2f9ea360e0690bed9dee6eec27
Igorsampa <igor.silva@sou.inteli.br.edu>
2024-03-26 20:24:17 -0300

    aa

---
+++ 2024-T0012-IN01-G02-main/src/texts/rexonaTexts.js
@@ -0,0 +1,8 @@
+// Each line represent one Question options and answers
+const rexonaTexts =
+[
+  // Estrutura ['Question', ['option1', 'option2', 'option3', 'answer']]
+  ["Para coletar as marcas, você precisará responder questões ao decorrer do jogo. Entedeu?", ['A) Não', 'B) Sim', 'C) Não', 'B']],
+  ["Questao 2. Entedeu?", ['A) Sim', 'B) Não', 'C) Não', 'A']]
+];
+export default rexonaTexts;
