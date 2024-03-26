7c0b2bd44bbb4812886578607a95ffc8a794a9ab
Igorsampa <igor.silva@sou.inteli.br.edu>
2024-03-26 20:26:25 -0300

    b

--- 2024-T0012-IN01-G02-main/src/entities/enemies/purpleSlime.js
+++
@@ -1,9 +0,0 @@
-import Slime from "./Slime.js";
-
-// extends slime properties (used only to specify class, so tile can read and spawn enemy correctly)
-export default class PurpleSlime extends Slime {
-  constructor(scene, x, y, slime_name) {
-    super(scene, x, y, slime_name);
-    this.damage = 30
-  }
-}
